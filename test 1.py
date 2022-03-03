432b0545dace5432308eb766d3f59b290f90d880
turluchki <61362585+turluchki@users.noreply.github.com>
2022-03-03 12:07:05 +0200

    Proverka kak raboti visual studio

---
+++ test 1.py
@@ -0,0 +1,13 @@
+a = [5, 8, 2, 6, 1]
+for i in range(0, 5):
+    if a[i] % 2 == 0:
+        a[i] = 1
+    else:
+        a[i] = 0
+print(a)
+z = 0
+h=5
+for n in range(0,5):
+        z = a[n] * 2 ** (h-1) + z
+        h=h-1
+print(z)
